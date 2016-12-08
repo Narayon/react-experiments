d8cb81fdc4683f7e979529610396f9df3b63b756
Narayon <rpedro10@hotmail.com>
2016-12-08 19:53:14 +0000

    Make the break great again

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,10 +2,10 @@ var React = require('react');
 
 var About = (props) => {
   return (
-        <div>
+    <div>
       <h1 className="text-center">About</h1>
       <p>
-        But there's something about us I want to say <br>
+        But there's something about us I want to say <br/>
         Cause there's something between us anyway
       </p>
     </div>
