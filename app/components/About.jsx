ed38e00eab23d8b350e1ad6ebfbe57d1f7830119
Narayon <rpedro10@hotmail.com>
2016-12-08 19:48:12 +0000

    Break the about page text

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -5,7 +5,7 @@ var About = (props) => {
         <div>
       <h1 className="text-center">About</h1>
       <p>
-        But there's something about us I want to say
+        But there's something about us I want to say <br>
         Cause there's something between us anyway
       </p>
     </div>
